bc6158def0549dde1e40796fcca2c361a60587a4
Travis <travis@kuhl.co>
2013-11-03 19:13:18 -0800

    update

--- src/index.php
+++ src/index.php
@@ -3,16 +3,29 @@
 namespace bolthq;
 use \b;
 
-b::route('*', function(){
-    return b::render(array(
-        'file' => __DIR__.'/layout.template.php'
-    ));
-});
+class site extends \bolt\browser\controller\request {
 
-b::route('install', function(){
-    b::location("https://raw.github.com/traviskuhl/bolt/3.0/build/install");
-});
+    public static $routes = [
+        ['route' => '/']
+    ];
 
-b::route('download', function(){
-    b::location('https://github.com/traviskuhl/bolt/raw/3.0/build/bolt-latest.tar.gz');
-});
+    public function get() {
+        return $this->view("page.hbr");
+    }
+
+}
+
+
+// b::route('*', function(){
+//     return b::render(array(
+//         'file' => __DIR__.'/layout.template.php'
+//     ));
+// });
+
+// b::route('install', function(){
+//     b::location("https://raw.github.com/traviskuhl/bolt/3.0/build/install");
+// });
+
+// b::route('download', function(){
+//     b::location('https://github.com/traviskuhl/bolt/raw/3.0/build/bolt-latest.tar.gz');
+// });
